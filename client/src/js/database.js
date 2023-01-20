85b8bc3ab8833eeb1d35b1b99c973666037974f5
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-19 19:44:12 -0800

    add: method for saving data to indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,29 @@
-import { openDB } from 'idb';
+import { openDB } from 'idb'
 
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
+        console.log('jate database already exists')
+        return
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true })
+      console.log('jate database created')
     },
-  });
+  })
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const textDbData = await openDB('jate', 1)
+  const transaction = textDbData.transaction('jate', 'readwrite')
+  const store = transaction.objectStore('jate')
+  const DBrequest = store.put({ id: 1, value: content })
+  const result = await DBrequest
+  console.log('PUT request to indexDb')
+  console.log('Successfully saved data to the database', result.value)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error('getDb not implemented')
 
-initdb();
+initdb()
