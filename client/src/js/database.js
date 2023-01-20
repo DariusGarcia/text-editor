c4f7bf0c463722918005a9650bf82d9735d828db
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-19 19:47:30 -0800

    add: method for fetching ALL content from indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,18 +12,34 @@ const initdb = async () =>
     },
   })
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// method that accepts some text content and adds it to the database
 export const putDb = async (content) => {
   const textDbData = await openDB('jate', 1)
   const transaction = textDbData.transaction('jate', 'readwrite')
   const store = transaction.objectStore('jate')
   const DBrequest = store.put({ id: 1, value: content })
   const result = await DBrequest
-  console.log('PUT request to indexDb')
-  console.log('Successfully saved data to the database', result.value)
+  console.log('PUT request to indexDB')
+  result
+    ? console.log('Successfully saved data to the database', result.value)
+    : console.log('Failed: Data was not added to indexDB')
 }
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented')
+// method that gets all the content from the database
+export const getDb = async () => {
+  const textDbData = await openDB('jate', 1)
+  const transaction = textDbData.transaction('jate', 'readonly')
+  const store = transaction.objectStore('jate')
+  const DBrequest = store.get(1)
+  const result = await DBrequest
+  console.log('Fetching ALL data from the database')
+  result
+    ? console.log(
+        'Successfully fetched all data from the indexDB',
+        result.value
+      )
+    : console.log('Failed: Data was not found in the indexDB')
+  return result?.value
+}
 
 initdb()
