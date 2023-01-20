f263499f17ab24d3e0cdbfc8b88e83bbc97d395f
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-19 19:37:32 -0800

    add: htmlWebpackPlugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path')
+const { InjectManifest } = require('workbox-webpack-plugin')
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -11,20 +11,21 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E',
+      }),
     ],
 
     module: {
-      rules: [
-        
-      ],
+      rules: [],
     },
-  };
-};
+  }
+}
