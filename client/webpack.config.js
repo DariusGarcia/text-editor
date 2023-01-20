fc9016388da7cd3c25dda8f1cea0a9f18e668abd
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-19 19:37:58 -0800

    add: manifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,6 +22,28 @@ module.exports = () => {
         template: './index.html',
         title: 'J.A.T.E',
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
